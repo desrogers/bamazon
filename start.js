dcc9305e1bfae4952800c185ba701e5299f80acb
dezroge <desmondrogers2@gmail.com>
2019-04-03 00:45:11 -0500

    Initial commit

---
+++ start.js
@@ -0,0 +1,7 @@
+const inquirer = require('inquirer');
+
+        // inquirer.prompt([{
+        //     name: "purchase",
+        //     type: "input",
+        //     message: "What would you like to buy?",
+        // }])
