f5fe4aff5465be51f37031679680d235640dcd90
dezroge <desmondrogers2@gmail.com>
2019-04-05 00:51:32 -0500

    deleted start.js

--- start.js
+++
@@ -1,7 +0,0 @@
-const inquirer = require('inquirer');
-
-        // inquirer.prompt([{
-        //     name: "purchase",
-        //     type: "input",
-        //     message: "What would you like to buy?",
-        // }])
